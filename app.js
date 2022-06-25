7a375b1f71d0569fdc8a2282720204de11a57212
zachary webb <zachsavn@gmail.com>
2022-06-25 17:01:13 -0400

    Created checkAge function

--- app.js
+++ app.js
@@ -3,15 +3,31 @@ console.log("Hello World!\n==========\n");
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
 //create function that prints only odds
-function printOdds (count){
-  for (let i = 1; i <= count; i++) {
-    if (i % 2 == 1) {
-    console.log(i);
+function printOdds (count) {
+  for (let i = 0; i <= count; i++) {
+    if (count <= 0) {
+      console.log("Enter valid positive integer.")
+    }
+    else if (i % 2 == 1) {
+        console.log(i);
     } else {
-      continue;
+        continue;
     }
   }
 }
-printOdds(100);
+printOdds(0);
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
+
+function checkAge (userName, age) {
+  let aboveSixteen = `Congrats ${userName}, you can drive!`
+  let belowSixteen = `Sorry ${userName}, but you need to wait until you're 16.`
+
+  if (age >= 16) {
+    console.log(aboveSixteen);
+  } else {
+    console.log(belowSixteen);
+  }
+}
+
+checkAge( 15);
