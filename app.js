97502b3794c695e1b9c266d7ffc8a7f66ad98f8e
zachary webb <zachsavn@gmail.com>
2022-06-25 16:12:24 -0400

    Finished printOdds function

--- app.js
+++ app.js
@@ -2,7 +2,16 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
-
-
+//create function that prints only odds
+function printOdds (count){
+  for (let i = 1; i <= count; i++) {
+    if (i % 2 == 1) {
+    console.log(i);
+    } else {
+      continue;
+    }
+  }
+}
+printOdds(100);
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
